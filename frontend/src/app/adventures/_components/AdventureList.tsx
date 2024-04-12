54076c9bdac17cdafe9c9d7ac905bd09a550afc4
Luc Hebert <luc.hebert13@gmail.com>
2024-04-12 11:20:55 -0400

    progress

--- frontend/src/app/adventures/_components/AdventureList.tsx
+++ frontend/src/app/adventures/_components/AdventureList.tsx
@@ -7,20 +7,17 @@ interface AdventuresListProps {
 
 const AdventuresList = ({ adventures }: AdventuresListProps) => {
   return (
-    <div className="container mx-auto px-4 py-8">
-      <h1 className="text-3xl font-bold mb-4">Adventures</h1>
-      <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-        {adventures.map((adventure) => (
-          <li
-            key={adventure.id}
-            className="bg-white rounded-lg shadow-md p-6 hover:shadow-lg transition duration-300"
-          >
-            <h2 className="text-lg font-semibold mb-2">{adventure.name}</h2>
-            <p className="text-gray-600">{adventure.location}</p>
-          </li>
-        ))}
-      </ul>
-    </div>
+    <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+      {adventures.map((adventure) => (
+        <li
+          key={adventure.id}
+          className="bg-white rounded-lg shadow-md p-6 hover:shadow-lg transition duration-300"
+        >
+          <h2 className="text-lg font-semibold mb-2">{adventure.name}</h2>
+          <p className="text-gray-600">{adventure.location}</p>
+        </li>
+      ))}
+    </ul>
   );
 };
 
--- frontend/src/app/adventures/page.tsx
+++ frontend/src/app/adventures/page.tsx
@@ -1,26 +1,45 @@
 "use client";
 
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, Suspense } from "react";
 import { Adventure } from "../types/adventure";
 import { fetchAdventures } from "../pages/api/adventures";
 import AdventuresList from "./_components/AdventureList";
+import { resolve } from "path";
 
 const AdventuresPage = () => {
   const [adventures, setAdventures] = useState<Adventure[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
+        setIsLoading(true);
+        await new Promise((resolve) => setTimeout(resolve, 5000));
         const data = await fetchAdventures();
         setAdventures(data);
       } catch (error) {
         console.error("Error fetching data:", error);
+      } finally {
+        setIsLoading(false);
       }
     };
     fetchData();
   }, []);
 
-  return <AdventuresList adventures={adventures} />;
+  return (
+    <div className="container mx-auto px-4 py-8">
+      <h1 className="text-3xl font-bold mb-4">Adventures</h1>
+      {isLoading ? (
+        // Show loading spinner when isLoading is true
+        <div className="flex justify-center">
+          <div className="animate-spin rounded-full h-20 w-20 border-b-2 border-gray-900"></div>
+        </div>
+      ) : (
+        // Render adventures list when data is loaded
+        <AdventuresList adventures={adventures} />
+      )}
+    </div>
+  );
 };
 
 export default AdventuresPage;
