ed8f3b543682be31af023dbf0dbea29cd23c3c9e
Russell Jones <499552+jonesrussell@users.noreply.github.com>
2024-05-13 16:15:00 +0000

    npm install -D eslint-config-next@latest

--- app/adventures/[id]/page.tsx
+++ app/adventures/[id]/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useState, useEffect } from 'react';
-import { useRouter, usePathname, useSearchParams } from 'next/navigation';
+import { useRouter, usePathname } from 'next/navigation';
 import { Adventure } from '../../types/adventure';
 
 export default function AdventureDetails() {
